62d186386954398b9c95f89d2732a063acdf8330
Ong Song Yi <ong_song_yi@aiap.sg>
2025-03-11 17:13:54 +0800

    feat: added saving of metrics, llm model and embedding model for record

--- src/evaluation/evaluation_pipeline.py
+++ src/evaluation/evaluation_pipeline.py
@@ -1,12 +1,13 @@
+import json
 import logging
 import os
+from datetime import datetime
 from typing import Optional
 
 import omegaconf
 import pandas as pd
-import torch
+import pytz
 from dotenv import load_dotenv
-from inference.inference_pipeline import InferencePipeline
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain_openai.chat_models import ChatOpenAI
 from ragas import evaluate
@@ -18,6 +19,8 @@ from ragas.metrics import (
     Faithfulness,
     LLMContextRecall,
 )
+
+from inference.inference_pipeline import InferencePipeline
 
 
 class EvaluationPipeline:
@@ -49,30 +52,7 @@ class EvaluationPipeline:
         self.ragas_df: Optional[pd.DataFrame] = None
         self.evaluator_llm: Optional[LangchainLLMWrapper] = None
         self.metrics: list[object] = []
-
-    def _load_embedding_model(self):
-        """
-        Loads the embedding model based on the configuration settings.
-
-        Raises:
-            RuntimeError: If the embedding model fails to load.
-        """
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-
-        try:
-            self.embedding_model = HuggingFaceInstructEmbeddings(
-                model_name=self.cfg.embeddings.embeddings_model.model_name,
-                show_progress=self.cfg.show_progress,
-                model_kwargs={"device": device},
-            )
-            self.logger.info(
-                f"{self.cfg.embeddings.embeddings_model.model_name} loaded to {device.upper()}"
-            )
-        except Exception as e:
-            self.logger.error(f"Failed to load embedding model: {e}")
-            raise RuntimeError(
-                "Embedding model failed to load. Please check configuration."
-            ) from e
+        self.no_of_questions: Optional[int] = None
 
     def _run_inference(self):
         """
@@ -83,7 +63,8 @@ class EvaluationPipeline:
         """
         try:
             infer_pipeline = InferencePipeline(cfg=self.cfg, logger=self.logger)
-            self.ragas_df = infer_pipeline.run_inference()
+            self.embedding_model = infer_pipeline.load_embedding_model()
+            self.ragas_df, self.no_of_questions = infer_pipeline.run_inference()
         except Exception as e:
             self.logger.error(f"Failed to run inference: {e}")
             raise RuntimeError(
@@ -160,14 +141,51 @@ class EvaluationPipeline:
         Raises:
             RuntimeError: If any component fails during execution.
         """
-        self._load_embedding_model()
         self._run_inference()
         self._initialize_llm()
         self._setup_metrics()
 
         try:
+            self.logger.info("Evaluating results.\n")
             results = evaluate(dataset=self.ragas_df, metrics=self.metrics)
             self.logger.info(f"Results: {results}")
+
+            sgt_timezone = pytz.timezone(self.cfg.timezone)
+            timestamp_sgt = datetime.now(sgt_timezone).strftime(
+                self.cfg.datetime_format
+            )
+
+            results_df = results.to_pandas()
+            metrics_list = results_df.columns[-len(self.metrics) :]
+            results_dict = results_df[metrics_list].mean().to_dict()
+
+            evaluation_entry = {
+                "timestamp": timestamp_sgt,
+                "no_of_questions": self.no_of_questions,
+                "embedding_model": self.cfg.embeddings.embeddings_model.model_name,
+                "llm": self.cfg.model,
+                "metrics": results_dict,
+            }
+
+            evaluation_log = os.path.join(self.cfg.metric_path, "evaluation_log.json")
+            if os.path.exists(evaluation_log):
+                with open(evaluation_log, "r", encoding="utf-8") as f:
+                    try:
+                        log_data = json.load(f)
+                        if not isinstance(log_data, list):
+                            log_data = []
+                    except json.JSONDecodeError:
+                        log_data = []
+
+            else:
+                log_data = []
+
+            log_data.append(evaluation_entry)
+
+            with open(evaluation_log, "w", encoding="utf-8") as f:
+                json.dump(log_data, f, indent=4)
+                self.logger.info(f"Metrics saved at {evaluation_log}")
+
         except Exception as e:
             self.logger.error(f"Evaluation failed: {e}")
             raise RuntimeError(
