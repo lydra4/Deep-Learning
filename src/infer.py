810a1c96a98224035c9e2e87642cbca1e4e71b09
Ong Song Yi <lydra4@outlook.com>
2025-04-07 12:20:27 +0800

    feat: 1st iteration of infer script

---
+++ src/infer.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+import hydra
+import omegaconf
+import torch
+from utils.general_utils import setup_logging
+from utils.seed_utils import fix_seed
+
+
+@hydra.main(version_base=None, config_path="../conf", config_name="inference.yaml")
+def main(cfg: omegaconf.DictConfig):
+    logger = logging.getLogger(__name__)
+    logger.info("Setting up logging configuration.\n")
+    setup_logging(
+        logging_config_path=os.path.join(
+            hydra.utils.get_original_cwd(), "conf", "logging.yaml"
+        )
+    )
+
+    if cfg.environ.seed:
+        logger.info(f"Random set to {cfg.environ.seed}.\n")
+        fix_seed(seed=cfg.environ.seed)
+
+    if cfg.environ.device < 0:
+        device = torch.device("cpu")
+    else:
+        device = torch.device(f"cuda:{cfg.environ.device}")
+    logger.info(f"Device set to {device} for inference.\n")
